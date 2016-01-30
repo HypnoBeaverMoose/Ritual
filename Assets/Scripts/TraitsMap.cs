616bc4b7996332d91599b5f34be421db2894cc6b
Yasen Atanasov <y.atanasoff@gmail.com>
2016-01-30 12:45:50 +0200

    Viliger Generator Prefab, Viliger Prefab Prototype scene

--- Assets/Scripts/TraitsMap.cs
+++ Assets/Scripts/TraitsMap.cs
@@ -4,24 +4,24 @@ using System.Collections;
 
 public enum GoodTraits { Kids = 0, Rain, Crops, Sunshine, Health }
 public enum BadTraits { NoKids = 0, Drought, Locust, Cold, Plague }
-public enum ViligerTraits { Virgin = 0, Killer, OldMan, Parent, Gipsy, }
+public enum ViligerTraits { Virgin = 0, Killer, OldMan, Parent, Gipsy }
 
 
 public static class TraitsMap
 {
-    GoodTraits GetGoodTrait(BadTraits trait)
+    static GoodTraits GetGoodTrait(BadTraits trait)
     {
         var index =(int)trait;
         return (GoodTraits)index;
     }
 
-    BadTraits GetBadTrait(GoodTraits trait)
+    static BadTraits GetBadTrait(GoodTraits trait)
     {
         var index = (int)trait;
         return (BadTraits)index;
     }
 
-    GoodTraits GetGoodTrait(ViligerTraits trait)
+    static GoodTraits GetGoodTrait(ViligerTraits trait)
     {
         var index = (int)trait;
         return (GoodTraits)index;
--- Assets/Scripts/Viliger.cs
+++ Assets/Scripts/Viliger.cs
@@ -1,17 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Viliger : MonoBehaviour {
 
+    public Image Icon;
+    public Text Message;
 
-    public Sprite Image;
+    public Sprite Image {get; set;}
     public ViligerTraits Trait { get; set; }
-    public string Text;
+    public string Text {get; set;}
 
- 
     void Start () 
     {
-	
+        Message.text = Text;
+        Icon.sprite = Image;
 	}
 	
 	void Update () {
